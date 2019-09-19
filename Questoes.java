63a760f882ef8a3e7e19c4b556c5628d96e60ca5
lps08 <lucianolps08@gmail.com>
2019-09-18 22:36:03 -0300

    q9-q22

--- Questoes.java
+++ Questoes.java
@@ -6,15 +6,7 @@ public class Questoes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//q1();
-		//q2();
-		//q3();
-		//q4();
-		//q5();
-		//q6();
-		//q7();
-		q8();
-
+		q22();
 	}
 	
 	static void q1 () {
@@ -97,4 +89,174 @@ public class Questoes {
 
 	}
 
+	static void q9 () {
+		System.out.print("Custo carro: ");
+		double custoCarro = sc.nextDouble();
+
+		System.out.println("Preço final: " + (custoCarro + (custoCarro * 0.73)));
+	}
+
+	static void q10 () {
+		int A = 10, B = 20, c;
+		c = A;
+		A = B;
+		B = c;
+		System.out.println("A: " + A + " B: " + B);
+	}
+
+	static void q11 () {
+		System.out.println("LETRA A");
+		int a = 100, b = 120;
+		System.out.println(b);
+		b = 234;
+		System.out.println(a + " " + b);
+		
+		System.out.println("LETRA B");
+		a = 23;
+		b = 56;
+		int c = a + b;
+		b = 34;
+		a = 67;
+		System.out.println(a + " " + b + " " + c);
+
+		System.out.println("LETRA C");
+		a = 300;
+		b = 200;
+		c = a + b;
+		System.out.println(c);
+		b = 140;
+		System.out.println(b + " " + c);
+		c = a + b;
+		System.out.println(a + " " + b + " " + c);
+		
+		/* ERROR
+		System.out.println("LETRA D");
+		int x = 1, y = 23;
+		int z = y - z;
+		System.out.println(z);
+		x = 5
+		y = x + z; */
+	}
+
+	static void q12 () {
+		double a = (4/2) + (2/4);
+		double A = 4/2 + 2/4;
+		double b = 4/(2+2)/4;
+		double B = 4/2 + 2/4;
+		double c = (4+2)*2-4;
+		double C = 4+2*2-4;
+		if (a == A) System.out.println("A - Igual");
+		else System.out.println("A - Diferente");
+		if (b == B) System.out.println("B - Igual");
+		else System.out.println("B - Diferente");
+		if (c == C) System.out.println("C - Igual");
+		else System.out.println("C - Diferente");
+	}
+
+	static void q13 () {
+		int a = 6*(3+2);
+		System.out.println(a);
+		int b = 6 * (3+2) + 2;
+		System.out.println(b);
+		int c = 2 + 3 * 6 / (2 + 4);
+		System.out.println(c);
+		int d = 2*8/(3+1);
+		System.out.println(d);
+		int e = 3+(16-2)/(2*(9-2));
+		System.out.println(e);
+		int f = 6/3 + 8/2;
+		System.out.println(f);
+		int g = (3+8/2)*4+3*2;
+		System.out.println(g);
+		int h = 6*3*3+6-10;
+		System.out.println(h);
+		int i = (10*8+3)*9;
+		System.out.println(i);
+		int j = -12*-4+3*-4;
+		System.out.println(j);
+	}
+
+	static void q14 () {
+		System.out.print("Numero: ");
+		int valor = sc.nextInt();
+		System.out.format("Antecessor: %d | Sucessor: %d\n", valor - 1, valor + 1);
+	}
+
+	static void q15 () {
+		System.out.print("Base retângulo: ");
+		double base = sc.nextDouble();
+		System.out.print("Altura retângulo: ");
+		double altura = sc.nextDouble();
+		System.out.println("Área total: " + (base * altura));
+	}
+
+	static void q16 () {
+		System.out.print("Anos: ");
+		int anos = sc.nextInt();
+		System.out.print("Meses: ");
+		int meses = sc.nextInt();
+		System.out.print("Dias: ");
+		int dias = sc.nextInt();
+
+		System.out.println("Idade em dias: " + ((365 * anos) + (meses * 30) + dias));
+	}
+
+	static void q17 () {
+		System.out.print("Total de Eleitores: ");
+		double totalEleitores = sc.nextDouble();
+		System.out.print("Votos brancos: ");
+		double votosBrancos = sc.nextDouble();
+		System.out.print("Votos nulos: ");
+		double votosNulos = sc.nextDouble();
+		System.out.print("Votos válidos: ");
+		double votosValidos = sc.nextDouble();
+
+		System.out.println("pert votos brancos: " + (totalEleitores/100) * votosBrancos  + " %");
+		System.out.println("pert votos nulos: " + (totalEleitores/100) * votosNulos + " %");
+		System.out.println("pert votos validos: " + (totalEleitores/100) * votosValidos + " %");
+	}
+
+	static void q18 () {
+		System.out.print("Salario mensal atual: ");
+		double salarioAtual = sc.nextInt();
+		System.out.print("Percentual reajuste: ");
+		double percReajuste = sc.nextDouble();
+
+		System.out.println("Novo salário: " + (salarioAtual + (salarioAtual * (percReajuste / 100))));
+	}
+
+	static void q19 () {
+		System.out.print("Custo do carro: ");
+		double custoCarro = sc.nextDouble();
+		custoCarro += custoCarro * 0.28;
+		custoCarro += custoCarro * 0.45;
+		System.out.println("Preço carro consumidor: " + custoCarro);
+	}
+
+	static void q20 () {
+		System.out.print("Numero de carros vendidos: ");
+		int numCarrosVendidos = sc.nextInt();
+		System.out.print("Valor total das vendas: ");
+		double valorTotalVendas = sc.nextDouble();
+		System.out.print("Salario fixo: ");
+		double salarioFixo = sc.nextDouble();
+		System.out.print("Valor por carro vendido: ");
+		double valorCadaCarroVendido = sc.nextDouble();
+
+		System.out.println("Salario final: " + (salarioFixo + (numCarrosVendidos * valorCadaCarroVendido) + (valorTotalVendas * 0.05)));
+	}
+
+	static void q21 () {
+		System.out.print("Valor: ");
+		double valor = sc.nextDouble();
+		if (valor > 10) System.out.println("MAIOR QUE 10!");
+		else System.out.println("NÃO É MAIOR QUE 10");
+	}
+
+	static void q22 () {
+		System.err.print("Valor: ");
+		double valor = sc.nextDouble();
+		if (valor > 0) System.out.println("Positivo");
+		else System.out.println("Negativo");
+	}	
 }
