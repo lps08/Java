2041fa1336b4b72f9652a1340467f5afd4b275c3
lps08 <lucianolps08@gmail.com>
2019-09-26 10:38:39 -0300

    qst1

---
+++ exercicio 2/qst1/questao1/src/app/Questao1.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Questao1 {
+    public static void main(String[] args) throws Exception {
+        
+        Lampada lampada = new Lampada();
+        lampada.liga();
+        lampada.observar();
+        lampada.desliga();
+        lampada.observar();
+
+    }
+}
