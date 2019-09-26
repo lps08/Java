e69b70833440358b21716f837d26e10e4caafed5
lps08 <lucianolps08@gmail.com>
2019-09-26 11:03:34 -0300

    qst3

---
+++ exercicio 2/questao3/src/app/Quadrado.java
@@ -0,0 +1,17 @@
+package app;
+
+public class Quadrado {
+    double lado;
+
+    public Quadrado (double ladoQuadrado) {
+        lado = ladoQuadrado;
+    }
+
+    public double area () {
+        return (lado * lado);
+    }
+
+    public double perimetro () {
+        return (4 * lado);
+    }
+}
---
+++ exercicio 2/questao3/src/app/Questao3.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Questao3 {
+    public static void main(String[] args) throws Exception {
+        
+        Quadrado quadrado = new Quadrado(2);
+        System.out.println("Area: " + quadrado.area());
+        System.out.println("Periemtro: " + quadrado.perimetro()); 
+       
+    }
+}
